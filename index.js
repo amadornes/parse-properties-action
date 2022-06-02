fdc9aeed9af9830158b692073cd4cc9bf848904a
amadornes <amadornes@gmail.com>
2022-06-02 15:43:29 +0200

    Whoops, overlapping names

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const core = require('@actions/core');
-const props = require('properties-file');
+const properties = require('properties-file');
 const fs = require('fs/promises');
 
 try {
   const file = core.getInput('file');
   fs.readFile(file, 'utf-8').then(contents => {
-    const props = props.parse(contents);
+    const props = properties.parse(contents);
     Object.entries(props).forEach(([key, val]) => {
       core.setOutput(key, val);
     });
